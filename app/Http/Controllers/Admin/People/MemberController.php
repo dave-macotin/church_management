5ff73e58f73146c7b790428d3a5aab59d8f4a421
dave macotin <macotindave@gmail.com>
2026-05-09 23:10:24 +0800

    login error

--- app/Http/Controllers/Admin/People/MemberController.php
+++ app/Http/Controllers/Admin/People/MemberController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\People\Member;
 use App\Models\People\Family;
 use App\Models\Role;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
